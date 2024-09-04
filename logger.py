aee8f8478a34ae9ae5a384a7471d33b1f4963705
aigizlo <aikido3103@gmail.com>
2024-09-05 02:44:43 +0400

    uft loger

--- logger.py
+++ logger.py
@@ -68,7 +68,7 @@
 # console_handler.setFormatter(formatter)
 import logging
 import sys
-
+import io
 # Настраиваем логгер
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)  # Установите уровень логирования на наивысший уровень, который вам нужен
@@ -78,9 +78,8 @@ file_handler = logging.FileHandler('bot.log', encoding='utf-8')
 file_handler.setLevel(logging.DEBUG)  # Уровень для записи в файл (в данном случае, установлен на DEBUG)
 
 # Добавляем консольный хендлер логов
-console_handler = logging.StreamHandler(sys.stdout)
+console_handler = logging.StreamHandler(io.TextIOWrapper(sys.stdout, encoding='utf-8'))
 console_handler.setLevel(logging.DEBUG)
-console_handler.encoding = 'utf-8'  # Set encoding to utf-8
 
 # Создаем форматтер для хендлеров
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
