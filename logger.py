d4d12562188c0b2f2b6278f45c14dd0048dd1fa6
aigizlo <aikido3103@gmail.com>
2024-09-05 03:01:40 +0400

    uft loger

--- logger.py
+++ logger.py
@@ -1,25 +1,25 @@
-import logging
-
-# Настраиваем логгер
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)  # Установите уровень логирования на наивысший уровень, который вам нужен
-
-# Добавляем файловый хендлер логов
-file_handler = logging.FileHandler('bot.log')
-file_handler.setLevel(logging.DEBUG)  # Уровень для записи в файл (в данном случае, установлен на DEBUG)
-logger.addHandler(file_handler)
-
-
-# Добавляем консольный хендлер логов
-console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.DEBUG)  # Уровень для вывода на консоль (в данном случае, установлен на DEBUG)
-logger.addHandler(console_handler)
-
-# Опционально, настройка формата логов
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-file_handler.setFormatter(formatter)
-console_handler.setFormatter(formatter)
-logger = logging.getLogger(__name__, encoding='utf-8')
+# import logging
+#
+# # Настраиваем логгер
+# logger = logging.getLogger(__name__)
+# logger.setLevel(logging.DEBUG)  # Установите уровень логирования на наивысший уровень, который вам нужен
+#
+# # Добавляем файловый хендлер логов
+# file_handler = logging.FileHandler('bot.log')
+# file_handler.setLevel(logging.DEBUG)  # Уровень для записи в файл (в данном случае, установлен на DEBUG)
+# logger.addHandler(file_handler)
+#
+#
+# # Добавляем консольный хендлер логов
+# console_handler = logging.StreamHandler()
+# console_handler.setLevel(logging.DEBUG)  # Уровень для вывода на консоль (в данном случае, установлен на DEBUG)
+# logger.addHandler(console_handler)
+#
+# # Опционально, настройка формата логов
+# formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# file_handler.setFormatter(formatter)
+# console_handler.setFormatter(formatter)
+# logger = logging.getLogger(__name__, encoding='utf-8')
 # # #
 # # import logging
 # #
@@ -89,3 +89,23 @@ logger = logging.getLogger(__name__, encoding='utf-8')
 # # Добавляем хендлеры к логгеру
 # logger.addHandler(file_handler)
 # logger.addHandler(console_handler)
+import logging
+
+# Настраиваем логгер
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)  # Установите уровень логирования на наивысший уровень, который вам нужен
+
+# Добавляем файловый хендлер логов
+file_handler = logging.FileHandler('bot.log', encoding='utf-8')  # Set encoding here!
+file_handler.setLevel(logging.DEBUG)  # Уровень для записи в файл (в данном случае, установлен на DEBUG)
+logger.addHandler(file_handler)
+
+# Добавляем консольный хендлер логов
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)  # Уровень для вывода на консоль (в данном случае, установлен на DEBUG)
+logger.addHandler(console_handler)
+
+# Опционально, настройка формата логов
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+console_handler.setFormatter(formatter)
