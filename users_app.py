7f6b0ddd31c93d18ff245bb1be20c0db0f6d1e7f
aigizlo <aikido3103@gmail.com>
2024-09-10 13:23:47 +0400

    count_users

--- users_app.py
+++ users_app.py
@@ -11,7 +11,8 @@ def index():
     links = user_data.show_links_info()
     sale_stats = sub.get_sale_stats()
     sale_paracents = sub.sale_paracent(sale_stats)
-    return render_template('index.html', users=user_data_, links=links, sale_stats=sale_stats, sale_paracents = sale_paracents)
+    count = user_data.all_users()
+    return render_template('index.html', users=user_data_, links=links, sale_stats=sale_stats, sale_paracents = sale_paracents, count = count)
 
 
 if __name__ == '__main__':
